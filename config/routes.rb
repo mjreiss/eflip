7403aba209204a156bca3994d3b73f97a619bb76
Mike <mjreiss4@gmail.com>
2015-05-21 01:04:27 -0400

    aesthetic designs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/home'
+  get   'pages/home'
+  get   'signup' => 'devise/registration#new'
+  post  'signup'   => 'devise/registration#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
