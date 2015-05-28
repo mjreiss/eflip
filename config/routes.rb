703af7a6fb98653e9536a7a6d3402ea3a5d0c06a
Mike <mjreiss4@gmail.com>
2015-05-28 14:05:07 -0400

    dashboard initial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :books
 
   devise_for :users
-  get   'pages/home'
+  get 'dashboard' => "pages#dashboard"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
